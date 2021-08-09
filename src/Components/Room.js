a5c6206a1a592737f2ea8ea5e88ce0d3ee7c5ece
V3cT0RX <karananantpure@gmail.com>
2021-08-09 20:17:17 +0530

    chat function implemented

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import axios from 'axios';
 import { io } from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
+// const ENDPOINT = "http://127.0.0.1:4001";
+const socket = io(`http://127.0.0.1:4001/`, { transports: ['websocket'] });
 
 class Room extends React.Component {
     constructor(props) {
@@ -11,46 +11,57 @@ class Room extends React.Component {
             isJoin: false,
         }
     }
+
     handleChange = event => {
         this.setState({ userName: event.target.value });
     }
     setUsername = event => {
-
         const joinMeeting = (socket, _meeting_id, userName) => {
             let message = {
                 type: 'JOIN',
                 data: {
                     meetingId: _meeting_id,
                     userName: userName
-                }
+                },
             };
             socket.emit("message", message);
+            // #region 
+            // socket.on("message", msg => {
+            //     switch (msg.type) {
+            //         case '_JOIN':
+            //             try {
+            //                 let message = msg.data;
+            //                 this.setState({ isJoin: true });
+            //                 console.log(message, this.state.isJoin);
+            //             } catch (err) {
+            //                 console.log(err, 'Unable to join room');
+            //                 socket.on("_ROOM_FULL", (message) => { console.log(message, 'Room full') });
+            //             }
+            //             break
+            //         default:
+            //             console.log('Invalid type');
+            //             break;
+            //     }
+            // });
+            // #endregion
+
             socket.on("_JOIN", (data) => {
                 try {
                     this.setState({ isJoin: true });
-                    console.log(data, this.state.isJoin);
-                    // this.props.history.push({
-                    //     pathname: '/chat',
-                    //     state: {
-                    //         meetId: data,
-                    //     }
-                    // })
                 } catch (err) {
                     console.log(err, 'Unable to join room');
                     socket.on("_ROOM_FULL", (data) => { console.log(data, 'Room full') });
                 }
             });
-        }
-        const socket = io(`http://127.0.0.1:4001/`, { transports: ['websocket'] });
-        console.log("Joinin meeting ", this.props.location.state.meetId);
-        joinMeeting(socket, this.props.location.state.meetId, this.props.location.state.userName);
-    };
 
+        }
+        joinMeeting(socket, this.props.location.state.meetId, this.state.userName);
+    };
 
     render() {
         if (this.state.isJoin) {
             return (
-                <Chat />
+                <Chat userName={this.state.userName} meetId={this.props.location.state.meetId} />
             );
         }
         else {
@@ -84,10 +95,23 @@ class Room extends React.Component {
 class Chat extends React.Component {
     constructor(props) {
         super(props);
+        this.userName = this.props.userName;
+        this.meetId = this.props.meetId;
         this.state = {
             msg: '',
             chat: [],
         }
+    }
+    componentDidMount() {
+
+        socket.on('_CHAT', data => {
+            console.log('no');
+            this.state.chat.push({ by: data.UserName, msg: data.msgData })
+            this.setState({ chat: this.state.chat });
+        });
+        // this.userName = this.props.location.state.userName;
+        // this.meetId = this.props.location.state.meetId
+
     }
     handleMsgChange = (e) => {
         this.setState({ msg: e.target.value });
@@ -95,39 +119,41 @@ class Chat extends React.Component {
     sendMessage = (event) => {
         event.preventDefault();
         let message = {
-            type: 'MESSAGE',
+            type: 'CHAT',
             data: {
-                userName: this.userName,
-                meetId: this.meetId,
+                UserName: this.userName,
+                meetingId: this.meetId,
                 msg: this.state.msg
             }
         };
         socket.emit("message", message);
+        this.state.chat.push({ by: 'me', msg: this.state.msg })
+        this.setState({ chat: this.state.chat });
+
         // this.socket.emit('msgSend', { userName: this.userName, meetId: this.meetId, msg: this.state.msg });
         this.setState({ msg: '' });
     }
     render() {
-        // const chats = this.state.chat.map(chat => {
-        //     return (
-        //         <div className="d-flex justify-content-between">
-        //             <div style={{ fontWeight: 'bolder' }}>
-        //                 {/* {chat.by} */} name
-        //             </div>
-        //             <div>
-        //                 {/* {chat.msg} */}msg
-        //             </div>
-        //         </div>
-        //     )
-        // })
+        const chats = this.state.chat.map(chat => {
+            return (
+                <div className="d-flex justify-content-between">
+                    <div style={{ fontWeight: 'bolder' }}>
+                        {chat.by}
+                    </div>
+                    <div>
+                        {chat.msg}
+                    </div>
+                </div>
+            )
+        })
         return (
             <div className="d-flex  align-items-center flex-column w-100 ">
                 {/* < div className="card" style={{ width: "50%" }}> */}
                 <div style={{ width: '40%' }}>
-                    {/* <div>{chats}</div> */}
+                    <div>{chats}</div>
                 </div>
                 <div className="d-flex  justify-content-center w-100 fixed-bottom">
                     <form className="d-flex w-50 " onSubmit={this.sendMessage}>
-                        {/* <label className="form-label" htmlFor="meeting">Enter Message</label> */}
                         <div className="mx-1 mb-3 w-100  ">
                             <input
                                 className=" w-100"
@@ -152,17 +178,3 @@ class Chat extends React.Component {
     }
 }
 export default Room;
-
-
-
-
-
-// socket.on('userExists', data => {
-    //     alert(data);
-    // });
-    // socket.on('userSet', data => {
-    //     this.props.history.push({
-    //         pathname: '/chat',
-    //         state: { meetId: this.props.location.state.meetId, userName: this.state.userName }
-    //     })
-    // })
